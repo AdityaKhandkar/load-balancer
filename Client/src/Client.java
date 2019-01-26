d856d494a8a00b62d249b8c89310dc27a8cc926b
Aditya Khandkar <adiya.s.khandkar@gmail.com>
2019-01-25 22:23:43 -0500

    Initial Client Server connection made

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,5 +1,23 @@
+import org.apache.xerces.impl.xs.SchemaNamespaceSupport;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by Aditya on 1/17/2019.
  */
 public class Client {
+    public static void main(String[] args) throws IOException {
+
+        // Sending number to server
+        Socket socket = new Socket("127.0.0.1", 1342);
+        System.out.println("Enter a number:");
+        int num = new Scanner(System.in).nextInt();
+        new PrintStream(socket.getOutputStream()).println(num);
+
+        // Receive a reply from the server
+        System.out.println(new Scanner(socket.getInputStream()).nextInt());
+    }
 }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,5 +1,22 @@
 /**
  * Created by Aditya on 1/17/2019.
  */
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+
+        // Accept the number sent by the client
+        ServerSocket srvS = new ServerSocket(1342);
+        Socket s = srvS.accept();
+        Scanner sc = new Scanner(s.getInputStream());
+        int inNum = sc.nextInt();
+
+        // Double the number and send it back
+        new PrintStream(s.getOutputStream()).println(inNum * 2);
+    }
 }
