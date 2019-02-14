3259138160249e38947bf0f58d16e0b546f03d1d
Aditya Khandkar <adiya.s.khandkar@gmail.com>
2019-02-14 11:38:28 -0500

    Primitive load balancer added

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -8,16 +8,21 @@ import java.util.Scanner;
 /**
  * Created by Aditya on 1/17/2019.
  */
+
 public class Client {
     public static void main(String[] args) throws IOException {
 
-        // Sending number to server
-        Socket socket = new Socket("127.0.0.1", 1342);
-        System.out.println("Enter a number:");
-        int num = new Scanner(System.in).nextInt();
-        new PrintStream(socket.getOutputStream()).println(num);
+        Socket socket = new Socket("127.0.0.1", 1341);
 
-        // Receive a reply from the server
-        System.out.println(new Scanner(socket.getInputStream()).nextInt());
+        while(true) {
+            // Sending number to server
+            System.out.println("Enter a number:");
+            int num = new Scanner(System.in).nextInt();
+            System.out.println("Your num is: " + num);
+            new PrintStream(socket.getOutputStream()).println(num);
+            System.out.println("Waiting on reply...");
+            // Receive a reply from the load balancer
+            System.out.println(new Scanner(socket.getInputStream()).nextInt());
+        }
     }
 }
