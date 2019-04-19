91d36ed21f3ce1f687d8ba792ef7f365a33d0795
Aditya Khandkar <adiya.s.khandkar@gmail.com>
2019-04-18 21:46:29 -0400

    added sunlab load balancer

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -11,21 +11,23 @@ public class ClientMain {
     public static void main(String[] args) {
         String localhost = "localhost";
         String loadBalancerAddress = localhost;
+        String sunLabLoadBalancerAddress = "ada.hbg.psu.edu";
         final int loadBalancerPort = 6150;
 
         Random r = new Random();
 
-        ServerInfo loadBalancerInfo = new ServerInfo(loadBalancerAddress, loadBalancerPort);
+        ServerInfo loadBalancerInfo = new ServerInfo(sunLabLoadBalancerAddress, loadBalancerPort);
 
         ExecutorService pool = Executors.newFixedThreadPool(Server.THREAD_LIMIT + 5);
 
-        while(true) {
-            try {
+
+        try {
+            while(true) {
                 pool.execute(() ->
                         System.out.println(new Client().communicate(loadBalancerInfo, r.nextInt(20) + 10)));
-            } catch (Exception e) {
-                System.err.println("In ClientMain: " + e.getMessage());
             }
+        } catch (Exception e) {
+            System.err.println("In ClientMain: " + e.getMessage());
         }
     }
 }
